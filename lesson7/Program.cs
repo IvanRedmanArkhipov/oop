994a471359603e9e5cffefea292207cf260f5755
IvanArkhipov <redm4nredm4n@gmail.com>
2022-01-24 23:04:06 +0300

    lesson7

---
+++ lesson7/Program.cs
@@ -0,0 +1,11 @@
+﻿using lesson7;
+
+ACoder a1 = new ACoder();
+Console.WriteLine(a1.Encode("Эта программа работает"));
+Console.WriteLine(a1.Decode("Юуб рспдсбннб сбвпубжу"));
+
+public interface ICoder
+{
+    string Encode(string shifr);
+    string Decode(string shifr);
+}
